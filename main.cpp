d64e3b8df5f81e348be8850d8a9272464129a122
Porty-Eric <ericlesdsantos@gmail.com>
2021-05-09 01:23:31 -0300

    09/05/2021 01:23
    HotFix da versão do dia 08/05
    Implementação na struct e bool para registrar os finais.

--- main.cpp
+++ main.cpp
@@ -22,8 +22,7 @@ typedef struct estados
 {
     int idEstado;
     string nomeEstado;
-    int acceptInicial;
-    int acceptFinal;
+    bool acceptFinal=0;
 } Estados[10];
 
 int contaLinhas()
@@ -63,6 +62,7 @@ int contaFinais()
         entrada.close();
         return quantFinais;
     }
+    return 0;
     cout << "Ocorreu um erro ao abrir o arquivo, perdao pela inconveniencia." << endl;
     exit(1);
 }
@@ -96,7 +96,6 @@ int achaInicial()
     string linha;
     string subStringTempId;
     int posicaoElemento = 0;
-    int posicaoDoInicial = -1;
     entrada.open(arquivo);
     if (entrada.is_open())
     {
@@ -117,25 +116,23 @@ int achaInicial()
                     }
                     if (subStringTempId == compStringId)
                         posicaoElemento++;
-                    //linhas[contador]->frase = linha;
-                    //cout << linhas[contador]->frase << endl;
                 }
             }
         }
         entrada.close();
-        return posicaoDoInicial;
+        return -1;
     }
     cout << "Ocorreu um erro ao abrir o arquivo, perdao pela inconveniencia." << endl;
     exit(1);
 }
 
-void guardaFinais(finais[])
+int guardaFinais(int *finais)
 {
 
     string linha;
     string subStringTempId;
+    int contador = 0;
     int posicaoElemento = 0;
-    int posicaoDoInicial = -1;
     entrada.open(arquivo);
     if (entrada.is_open())
     {
@@ -149,61 +146,79 @@ void guardaFinais(finais[])
                     subStringTempId = linha.substr(0, 13);
                     if (subStringTempId == fecha)
                         break;
-                    if (subStringTempId == isInicial)
+                    subStringTempId = linha.substr(0, 11);
+                    if (subStringTempId == isFinal)
                     {
-                        entrada.close();
-                        return posicaoElemento;
+                        finais[contador] = posicaoElemento;
+                        contador++;
                     }
+                    subStringTempId = linha.substr(0, 13);
                     if (subStringTempId == compStringId)
                         posicaoElemento++;
-                    //linhas[contador]->frase = linha;
-                    //cout << linhas[contador]->frase << endl;
                 }
             }
         }
         entrada.close();
-        return posicaoDoInicial;
+        return *finais;
     }
     cout << "Ocorreu um erro ao abrir o arquivo, perdao pela inconveniencia." << endl;
     exit(1);
 }
 
-void mineraNomes(Estados *elem)
+void mineraElementos(Estados *elem)
 {
     string subStringTempId;
     string linha;
     int indice = 0;
     entrada.open(arquivo);
-    while (getline(entrada, linha))
+    if (entrada.is_open())
     {
-        subStringTempId = linha.substr(0, 13);
-        if (subStringTempId == compStringId)
+        while (getline(entrada, linha))
         {
-            int posicao1 = linha.find("name=\"") + 6;
-            int posicao2 = linha.find("\">");
-            int posicaoDiff = posicao2 - posicao1;
-            elem[indice]->idEstado = indice;
-            elem[indice]->nomeEstado = linha.substr(posicao1, posicaoDiff);
-            indice++;
+            subStringTempId = linha.substr(0, 13);
+            if (subStringTempId == compStringId)
+            {
+                int posicao1 = linha.find("name=\"") + 6;
+                int posicao2 = linha.find("\">");
+                int posicaoDiff = posicao2 - posicao1;
+                elem[indice]->idEstado = indice;
+                elem[indice]->nomeEstado = linha.substr(posicao1, posicaoDiff);
+                indice++;
+            }
         }
+        entrada.close();
+        return;
     }
+    cout << "Ocorreu um erro ao abrir o arquivo, perdao pela inconveniencia." << endl;
+    exit(1);
 }
 
 int main()
 {
-    Estados elem;
-    FraseLinhas frase;
-    //mineraNomes(&elem);
+    int quantElementos = contaElementos();
+    Estados elem[10]; //tamanho do automato pra ser alocado dinamicamente no futuro
+    mineraElementos(elem);
     int posicaoDoInicial = achaInicial();
     int quantFinais = contaFinais();
     int *elementosFinais = (int *)malloc(quantFinais * sizeof(int));
-    guardaFinais(&elementosFinais);
-    int quantElementos = contaElementos();
+    *elementosFinais = guardaFinais(elementosFinais);
     int quantLinhas = contaLinhas();
-    /*for (int i = 0; i < 5; i++)
+    
+    //Printa os elementos finais para testes
+    /*for (int i = 0; i < quantFinais; i++)
     {
-        cout << "Palavra " << i << frase[i].frase << endl;
+        cout << " Final: " << elementosFinais[i] ;
     }*/
+
+    //Printa as informações da struct para testes
+    /*for (int i = 0; i < quantElementos; i++)
+    {
+        string aceita = (elem[i]->acceptFinal == 1) ? "Sim" : "Nao";
+        cout << "Id do elemento: " << elem[i]->idEstado << endl
+             << "Nome do elemento: " << elem[i]->nomeEstado << endl
+             << "E final: " << aceita << endl;
+    }*/
+
     if (posicaoDoInicial != -1)
         cout << "ID do elemento que esta o inicial: " << posicaoDoInicial << endl;
     cout << "Quantidade de finais: " << quantFinais << endl;
@@ -212,13 +227,15 @@ int main()
     return 0;
 }
 
-//Criar um vetor de Strings com o tamanho de linhas que vamos precisar para poder escrever os epslon e o auxiliar dentro dele
+//AMANHÃ CRIAR UM BOOLEAN DENTRO DA STRUCT FALANDO SE É FINAL OU NÃO, SERÁ NECESSÁRIO
+
+//-Criar um vetor de Strings com o tamanho de linhas que vamos precisar para poder escrever os epslon e o auxiliar dentro dele
 //E sobrescrever o automato.jflap usando o vetor de strings no final
 
 //Criar menu
-//Opcao 1 - implementar função estrela no automato
-//Opcao 2 - implementar função complemento  automato
-//Opcao 3 - abrir o arquivo automato.jflap para apresentar ao usuário
+//-Opcao 1 - implementar função estrela no automato
+//-Opcao 2 - implementar função complemento  automato
+//-Opcao 3 - abrir o arquivo automato.jflap para apresentar ao usuário
 
 // Comportamento do lambda é um read vazio:
 //          <from>posicaoInicial</from>&#13;
@@ -229,12 +246,12 @@ int main()
 //- Criar um struct auxiliar com as mesmas definições do typedef Elementos e por ele como posicao incial e tirar posicao final do antigo
 //- O Auxiliar vai apontar para o antigo
 //- Descobrir a posição dos finais
-//Criar um auxiliar que vai apontar para o incial usando epslon
-//Fazer os finais apontarem para o inicial anterior usando  epslon
-//Guardar o valor da posição do inicial, e quando criar o auxiliar alterar apenas o valor de x, colocando a formula x = posicaoInicial-150
+//-Criar um auxiliar que vai apontar para o incial usando epslon
+//-Fazer os finais apontarem para o inicial anterior usando  epslon
+//-Guardar o valor da posição do inicial, e quando criar o auxiliar alterar apenas o valor de x, colocando a formula x = posicaoInicial-150
 
 //Complemento
-//Fazer os finais deixarem de serem inciais
-//Fazer os não finais virarem finais
+//-Fazer os finais deixarem de serem inciais
+//-Fazer os não finais virarem finais
 
-//Criar 2 arquivos, antigo e alterado
+//-Criar 2 arquivos, antigo e alterado
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,1 @@
-linha
+finais
